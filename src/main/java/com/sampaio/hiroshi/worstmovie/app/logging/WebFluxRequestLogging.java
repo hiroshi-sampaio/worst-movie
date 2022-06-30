e2f9152cc9d59fe8a4c567a68ba44f0973b7da0b
Hiroshi Sampaio <17440771+hiroshi-sampaio@users.noreply.github.com>
2022-06-30 17:39:42 -0300

    Webflux request headers logging

---
+++ src/main/java/com/sampaio/hiroshi/worstmovie/app/logging/WebFluxRequestLogging.java
@@ -0,0 +1,26 @@
+package com.sampaio.hiroshi.worstmovie.app.logging;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class WebFluxRequestLogging implements WebFilter {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
+
+        var request = exchange.getRequest();
+        log.trace("{} {} {}", request.getMethod(), request.getURI(),
+                request.getHeaders().toSingleValueMap().entrySet().stream()
+                        .map(e -> "\n" + e.getKey() + " = " + e.getValue())
+                        .collect(Collectors.joining()));
+        return chain.filter(exchange);
+    }
+}
